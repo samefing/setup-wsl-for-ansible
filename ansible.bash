02a03c2c752441dcfe5e05be33489b174e3d71bb
James Cruickshank <samefing@gmail.com>
2021-10-17 14:17:38 +0100

    working

--- ansible.bash
+++ ansible.bash
@@ -1,7 +1,7 @@
+export PATH="/home/$USER/.local/bin:$PATH"
 sudo apt update && sudo apt upgrade -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt install python3-pip -y && pip3 install --upgrade pip
-export PATH="/home/$USER/.local/bin:$PATH"
 pip3 install ansible
 curl -O https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
 pip3 install -r requirements-azure.txt
